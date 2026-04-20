09282577541d2535fd260381e23ac4ea503c422a
OtavioXimarelli <ximarellio@gmail.com>
2026-04-19 21:44:09 -0300

    feat: add MongoDB configuration class with auditing support

---
+++ src/main/java/dev/ximarelli/rosary/backend/config/MongoConfig.java
@@ -0,0 +1,10 @@
+package dev.ximarelli.rosary.backend.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.EnableMongoAuditing;
+
+@Configuration
+@EnableMongoAuditing
+public class MongoConfig {
+}
